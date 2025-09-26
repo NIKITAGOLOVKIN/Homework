7e502332c7d9e6acdc9e94a2a8e6ff8214cdd6ac
Nikita Golovkin <golovkinnv7@gmail.com>
2025-09-26 15:28:02 +0000

    Add second python homework

---
+++ src/bubble sorting.py
@@ -0,0 +1,11 @@
+from random import randint
+n = int(input('Введите длину массива '))
+a = [randint(0, 100) for i in range(n)]
+print('Массив до сортировки')
+print(a)
+for j in range(n):
+    for i in range(n - 1):
+        if a[i] > a[i + 1]:
+            a[i], a[i + 1] = a[i + 1], a[i]
+print('Массив после сортировки ')
+print(a)
